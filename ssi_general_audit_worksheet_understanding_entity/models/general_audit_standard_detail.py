132be112e8b634e36c5ee940f5948e25d5354220
Andhitia Rama <andhitia.r@gmail.com>
2024-09-23 17:57:41 +0700

    [UPD] ssi_general_audit_worksheet_understanding_entity
    * Menambahkan KK Main Business Activity Process
    * Menambahkan KK Business Cycle

--- ssi_general_audit_worksheet_understanding_entity/models/general_audit_standard_detail.py
+++ ssi_general_audit_worksheet_understanding_entity/models/general_audit_standard_detail.py
@@ -49,6 +49,58 @@ class GeneralAuditStandardDetail(models.Model):
         store=True,
         compute_sudo=True,
     )
+    ae11f7e_expert_ids = fields.Many2many(
+        string="AE11F7E Experts",
+        comodel_name="general_audit_ws_ae11f7e.expert",
+        relation="rel_general_audit_ws_ae11f7e_expert_2_standard_detail",
+        column1="standard_detail_id",
+        column2="expert_id",
+    )
+    expert_impacted = fields.Boolean(
+        string="Impacted By Use of Expert",
+        compute="_compute_expert_impacted",
+        store=True,
+        compute_sudo=True,
+    )
+    ae11f7e_previous_audit_information_ids = fields.Many2many(
+        string="AE11F7E Previous Significant Audit Information",
+        comodel_name="general_audit_ws_ae11f7e.previous_audit_information",
+        relation="rel_general_audit_ws_ae11f7e_prev_audit_2_standard_detail",
+        column1="standard_detail_id",
+        column2="prev_audit_id",
+    )
+    previous_audit_information_impacted = fields.Boolean(
+        string="Impacted By Previous Audit Information",
+        compute="_compute_previous_audit_information_impacted",
+        store=True,
+        compute_sudo=True,
+    )
+    ae11f7e_previous_other_information_ids = fields.Many2many(
+        string="AE11F7E Previous Significant Other Information",
+        comodel_name="general_audit_ws_ae11f7e.previous_other_information",
+        relation="rel_general_audit_ws_ae11f7e_prev_other_2_standard_detail",
+        column1="standard_detail_id",
+        column2="prev_other_id",
+    )
+    previous_other_information_impacted = fields.Boolean(
+        string="Impacted By Previous Other Information",
+        compute="_compute_previous_other_information_impacted",
+        store=True,
+        compute_sudo=True,
+    )
+    ae11f7e_other_information_ids = fields.Many2many(
+        string="AE11F7E Other Significant Information",
+        comodel_name="general_audit_ws_ae11f7e.other_information",
+        relation="rel_general_audit_ws_ae11f7e_other_2_standard_detail",
+        column1="standard_detail_id",
+        column2="other_id",
+    )
+    other_information_impacted = fields.Boolean(
+        string="Impacted By Other Information",
+        compute="_compute_other_information_impacted",
+        store=True,
+        compute_sudo=True,
+    )
 
     @api.depends(
         "a13a30e_detail_ids",
@@ -79,3 +131,43 @@ class GeneralAuditStandardDetail(models.Model):
             if record.c0e0eec_detail_ids:
                 result = True
             record.fraud_impacted = result
+
+    @api.depends(
+        "ae11f7e_expert_ids",
+    )
+    def _compute_expert_impacted(self):
+        for record in self:
+            result = False
+            if record.ae11f7e_expert_ids:
+                result = True
+            record.expert_impacted = result
+
+    @api.depends(
+        "ae11f7e_previous_audit_information_ids",
+    )
+    def _compute_previous_audit_information_impacted(self):
+        for record in self:
+            result = False
+            if record.ae11f7e_previous_audit_information_ids:
+                result = True
+            record.previous_audit_information_impacted = result
+
+    @api.depends(
+        "ae11f7e_previous_other_information_ids",
+    )
+    def _compute_previous_other_information_impacted(self):
+        for record in self:
+            result = False
+            if record.ae11f7e_previous_other_information_ids:
+                result = True
+            record.previous_other_information_impacted = result
+
+    @api.depends(
+        "ae11f7e_other_information_ids",
+    )
+    def _compute_other_information_impacted(self):
+        for record in self:
+            result = False
+            if record.ae11f7e_other_information_ids:
+                result = True
+            record.other_information_impacted = result
