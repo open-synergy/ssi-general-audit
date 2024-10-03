c39d11dd40262cb86d7a0b16ffbb16667dab5809
Andhitia Rama <andhitia.r@gmail.com>
2024-10-03 13:49:30 +0700

    [UPD] ssi_general_audit_worksheet_understanding_entity
    * Menambahkan informasi expert yang dibutuhkan pd general audit

--- ssi_general_audit_worksheet_understanding_entity/models/general_audit.py
+++ ssi_general_audit_worksheet_understanding_entity/models/general_audit.py
@@ -2,7 +2,7 @@
 # Copyright 2022 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class GeneralAudit(models.Model):
@@ -26,3 +26,29 @@ class GeneralAudit(models.Model):
         column1="general_audit_id",
         column2="other_report_id",
     )
+    ws_ae11f7e_expert_ids = fields.One2many(
+        string="WS AE11F7E Details",
+        comodel_name="general_audit_ws_ae11f7e.expert",
+        inverse_name="general_audit_id",
+    )
+    expert_type_ids = fields.Many2many(
+        string="Expert Needed",
+        comodel_name="general_audit_expert_type",
+        relation="rel_general_audit_2_expert_type",
+        column1="general_audit_id",
+        column2="expert_type_id",
+        compute="_compute_expert_type_ids",
+        store=True,
+        compute_sudo=True,
+    )
+
+    @api.depends(
+        "ws_ae11f7e_expert_ids",
+        "ws_ae11f7e_expert_ids.type_id",
+    )
+    def _compute_expert_type_ids(self):
+        for record in self:
+            result = []
+            if record.ws_ae11f7e_expert_ids:
+                result = record.mapped("ws_ae11f7e_expert_ids.type_id.id")
+            record.expert_type_ids = result
