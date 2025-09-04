fd03b1d23b72201f204692a6fe6d01efefddd579
mikevhe18 <viriyananda.michael@gmail.com>
2025-09-04 13:22:06 +0700

    Penambahan modul ssi_general_audit_core

---
+++ ssi_general_audit_core/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2025 OpenSynergy Indonesia
+# Copyright 2025 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+{
+    "name": "General Audit - Core",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "depends": [
+        "ssi_general_audit_worksheet_audit_working_plan",
+        "ssi_general_audit_worksheet_engagement_letter_checklist",
+        "ssi_general_audit_worksheet_inherent_risk",
+        "ssi_general_audit_worksheet_understanding_entity",
+        "ssi_general_audit_worksheet_planning_memorandum",
+        "ssi_general_audit_worksheet_control_risk",
+        "ssi_general_audit_worksheet_preliminary_materiality",
+        "ssi_general_audit_worksheet_romm",
+        "ssi_general_audit_worksheet_preliminary_analytic_procedure",
+    ],
+    "data": [],
+}
