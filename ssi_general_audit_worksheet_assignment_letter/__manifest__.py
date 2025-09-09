3a53d68f4f2b5abf5219451be82c65e8553dd545
mikevhe18 <viriyananda.michael@gmail.com>
2025-09-09 18:35:31 +0700

    [14.0.1.0.0] ssi_general_audit_worksheet_assignment_letter

---
+++ ssi_general_audit_worksheet_assignment_letter/__manifest__.py
@@ -0,0 +1,29 @@
+# Copyright 2025 OpenSynergy Indonesia
+# Copyright 2025 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "General Audit Worksheet - Assignment Letter",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_general_audit",
+    ],
+    "data": [
+        "security/ir_module_category_data.xml",
+        "data/general_audit_worksheet_type_data.xml",
+        # c435bcd
+        "security/res_group/general_audit_ws_c435bcd.xml",
+        "security/ir_rule/general_audit_ws_c435bcd.xml",
+        "data/ir_sequence/general_audit_ws_c435bcd.xml",
+        "data/sequence_template/general_audit_ws_c435bcd.xml",
+        "data/policy_template/general_audit_ws_c435bcd.xml",
+        "data/approval_template/general_audit_ws_c435bcd.xml",
+        "views/general_audit_ws_c435bcd_views.xml",
+        "security/ir.model.access.csv",
+    ],
+    "demo": [],
+}
---
+++ ssi_general_audit_worksheet_assignment_letter/models/general_audit_ws_c435bcd.py
@@ -0,0 +1,16 @@
+# Copyright 2025 OpenSynergy Indonesia
+# Copyright 2025 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+
+from odoo import models
+
+
+class GeneralAuditWSc435bcd(models.Model):
+    _name = "general_audit_ws_c435bcd"
+    _description = "Assignment Letter (c435bcd)"
+    _inherit = [
+        "general_audit_worksheet_mixin",
+    ]
+    _type_xml_id = (
+        "ssi_general_audit_worksheet_assignment_letter." "worksheet_type_c435bcd"
+    )
