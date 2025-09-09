9fee26da9508411babb746b49713f38502070bec
mikevhe18 <viriyananda.michael@gmail.com>
2025-09-09 18:31:07 +0700

    Update depends

--- ssi_general_audit_core/__manifest__.py
+++ ssi_general_audit_core/__manifest__.py
@@ -10,15 +10,31 @@
     "installable": True,
     "application": False,
     "depends": [
+        # PE
         "ssi_general_audit_worksheet_audit_working_plan",
         "ssi_general_audit_worksheet_engagement_letter_checklist",
-        "ssi_general_audit_worksheet_inherent_risk",
+        "ssi_general_audit_worksheet_acceptance_continuance",
+        "ssi_general_audit_worksheet_assignment_letter",
+        "ssi_general_audit_worksheet_independence_statement",
+        "ssi_general_audit_worksheet_team_communication",
+        # RA
         "ssi_general_audit_worksheet_understanding_entity",
-        "ssi_general_audit_worksheet_planning_memorandum",
-        "ssi_general_audit_worksheet_control_risk",
         "ssi_general_audit_worksheet_preliminary_materiality",
-        "ssi_general_audit_worksheet_romm",
         "ssi_general_audit_worksheet_preliminary_analytic_procedure",
+        "ssi_general_audit_worksheet_planning_memorandum",
+        "ssi_general_audit_worksheet_inherent_risk",
+        "ssi_general_audit_worksheet_control_risk",
+        "ssi_general_audit_worksheet_romm",
+        # RR
+        "ssi_general_audit_worksheet_lead_schedule",
+        "ssi_general_audit_worksheet_specific_procedure",
+        "ssi_general_audit_worksheet_expert",
+        # WR
+        "ssi_general_audit_worksheet_audit_result",
+        "ssi_general_audit_worksheet_final_materiality",
+        "ssi_general_audit_worksheet_draft_reporting",
+        "ssi_general_audit_worksheet_review",
+        "ssi_general_audit_worksheet_final_report",
     ],
     "data": [],
 }
