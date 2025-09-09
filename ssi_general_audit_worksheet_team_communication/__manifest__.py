d373759d105cb37679c7542e2dbf4b8d578b9cde
mikevhe18 <viriyananda.michael@gmail.com>
2025-09-09 18:31:29 +0700

    [14.0.1.0.0] ssi_general_audit_worksheet_team_communication

---
+++ ssi_general_audit_worksheet_team_communication/__manifest__.py
@@ -0,0 +1,29 @@
+# Copyright 2025 OpenSynergy Indonesia
+# Copyright 2025 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "General Audit Worksheet - Team Communication",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_general_audit",
+    ],
+    "data": [
+        "security/ir_module_category_data.xml",
+        "data/general_audit_worksheet_type_data.xml",
+        # 437fc8f
+        "security/res_group/general_audit_ws_437fc8f.xml",
+        "security/ir_rule/general_audit_ws_437fc8f.xml",
+        "data/ir_sequence/general_audit_ws_437fc8f.xml",
+        "data/sequence_template/general_audit_ws_437fc8f.xml",
+        "data/policy_template/general_audit_ws_437fc8f.xml",
+        "data/approval_template/general_audit_ws_437fc8f.xml",
+        "views/general_audit_ws_437fc8f_views.xml",
+        "security/ir.model.access.csv",
+    ],
+    "demo": [],
+}
