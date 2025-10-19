ede9b6a0dd3373dc1196056fb9e3eb8d1be0dcf9
Andhitia Rama <andhitia.r@gmail.com>
2025-10-19 16:10:04 +0700

    [UPD] ssi_general_audit_worksheet_lead_schedule
    * LEAD schedule

--- ssi_general_audit_worksheet_lead_schedule/models/f9f3299/general_audit_ws_f9f3299.py
+++ ssi_general_audit_worksheet_lead_schedule/models/f9f3299/general_audit_ws_f9f3299.py
@@ -2,7 +2,7 @@
 # Copyright 2025 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
 
-from odoo import models
+from odoo import api, fields, models
 
 
 class GeneralAuditWSf9f3299(models.Model):
@@ -12,3 +12,80 @@ class GeneralAuditWSf9f3299(models.Model):
         "general_audit_worksheet_mixin",
     ]
     _type_xml_id = "ssi_general_audit_worksheet_lead_schedule." "worksheet_type_f9f3299"
+
+    account_type_id = fields.Many2one(
+        comodel_name="client_account_type",
+        string="Standard Account Type",
+        required=True,
+        states={
+            "draft": [("readonly", False)],
+        },
+        readonly=True,
+        ondelete="restrict",
+    )
+    allowed_account_type_ids = fields.Many2many(
+        comodel_name="client_account_type",
+        string="Allowed Account Types",
+        help="Account types that can be selected",
+        compute="_compute_allowed_account_type_ids",
+        store=False,
+    )
+
+    balance_type = fields.Selection(
+        selection=[
+            ("end_period", "End Period Balance"),
+            ("interim", "Interim Balance"),
+        ],
+        string="Balance Type",
+        required=True,
+        default="end_period",
+        readonly=True,
+        states={
+            "draft": [("readonly", False)],
+            "open": [("readonly", False)],
+        },
+        help="Type of balance to consider for the accounts.",
+    )
+    detail_ids = fields.One2many(
+        comodel_name="general_audit_ws_f9f3299.detail",
+        inverse_name="worksheet_id",
+        string="Details",
+        readonly=True,
+        states={
+            "draft": [("readonly", False)],
+            "open": [("readonly", False)],
+        },
+        help="Detailed lines for each account in the lead schedule.",
+    )
+
+    @api.depends(
+        "general_audit_id",
+    )
+    def _compute_allowed_account_type_ids(self):
+        for record in self:
+            record.allowed_account_type_ids = self.env["client_account_type"]
+            if record.general_audit_id:
+                record.allowed_account_type_ids = record.general_audit_id.mapped(
+                    "standard_detail_ids.type_id"
+                )
+
+    @api.onchange(
+        "general_audit_id",
+    )
+    def onchange_account_type_id(self):
+        self.account_type_id = False
+
+    def action_reload_account(self):
+        for record in self.sudo():
+            record._reload_account()
+
+    def _reload_account(self):
+        self.ensure_one()
+        self.detail_ids.unlink()
+        for detail in self.general_audit_id.detail_id:
+            data = {
+                "worksheet_id": self.id,
+                "detail_id": detail.id,
+                "account_id": detail.account_id.id,
+            }
+            self.env["general_audit_ws_f9f3299.detail"].create(data)
---
+++ ssi_general_audit_worksheet_lead_schedule/models/f9f3299/general_audit_ws_f9f3299_detail.py
@@ -0,0 +1,132 @@
+# Copyright 2025 OpenSynergy Indonesia
+# Copyright 2025 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+
+from odoo import api, fields, models
+
+
+class GeneralAuditWSf9f3299Detail(models.Model):
+    _name = "general_audit_ws_f9f3299.detail"
+    _description = "Lead Schedule - Account (f9f3299) - Detail"
+
+    worksheet_id = fields.Many2one(
+        comodel_name="general_audit_ws_f9f3299",
+        string="# Worksheet",
+        required=True,
+        ondelete="cascade",
+    )
+    currency_id = fields.Many2one(
+        string="Currency",
+        comodel_name="res.currency",
+        related="worksheet_id.general_audit_id.currency_id",
+        store=True,
+        help="Currency of amounts; inherited from the parent worksheet.",
+    )
+    detail_id = fields.Many2one(
+        comodel_name="general_audit.detail",
+        string="# Detail",
+        required=True,
+        ondelete="cascade",
+    )
+    account_id = fields.Many2one(
+        string="Account",
+        comodel_name="client_account",
+        related="detail_id.account_id",
+        store=True,
+        readonly=True,
+        help="Client Account",
+    )
+    current_balance = fields.Monetary(
+        string="Current Balance",
+        compute="_compute_current_balance",
+        store=True,
+    )
+    supporting_schedule = fields.Char(
+        string="Supporting Schedule",
+        help="Reference to the supporting schedule for this account.",
+    )
+
+    adjustment_ids = fields.Many2many(
+        string="Adjustment Entries",
+        comodel_name="client_adjustment_entry",
+        related="detail_id.adjustment_ids",
+        help="Adjustment entries related to this account.",
+    )
+    adjustment_dr = fields.Monetary(
+        string="Total Debit Adjustments",
+        related="detail_id.adjustment_dr",
+        store=True,
+        help="Total debit adjustments for this account.",
+    )
+    adjustment_cr = fields.Monetary(
+        string="Total Credit Adjustments",
+        related="detail_id.adjustment_cr",
+        store=True,
+        help="Total credit adjustments for this account.",
+    )
+    adjusted_balance = fields.Monetary(
+        string="Adjusted Balance",
+        compute="_compute_adjusted_balance",
+        store=True,
+    )
+    previous_balance = fields.Monetary(
+        string="Previous Balance",
+        related="detail_id.previous_balance",
+        store=True,
+        help="Previous period balance for this account.",
+    )
+    diff = fields.Float(
+        string="Difference (%)",
+        compute="_compute_diff",
+        store=True,
+        help="Percentage difference between audited and previous balances.",
+    )
+
+    @api.depends(
+        "adjusted_balance",
+        "previous_balance",
+    )
+    def _compute_diff(self):
+        for record in self:
+            record.diff = 0.0
+            if record.previous_balance != 0:
+                record.diff = (record.adjusted_balance - record.previous_balance) / abs(
+                    record.previous_balance
+                )
+
+    @api.depends(
+        "worksheet_id.balance_type",
+        "detail_id",
+        "detail_id.home_statement_balance",
+        "detail_id.interim_balance",
+    )
+    def _compute_current_balance(self):
+        for record in self:
+            record.current_balance = 0.0
+            if record.worksheet_id.balance_type == "end_period":
+                record.current_balance = record.detail_id.home_statement_balance
+            else:
+                record.current_balance = record.detail_id.interim_balance
+
+    @api.depends(
+        "current_balance",
+        "adjustment_dr",
+        "adjustment_cr",
+        "account_id",
+    )
+    def _compute_adjusted_balance(self):
+        for record in self:
+            record.adjusted_balance = 0.0
+            if record.account_id:
+                if record.account_id.normal_balance == "debit":
+                    record.adjusted_balance = (
+                        record.current_balance
+                        + record.adjustment_dr
+                        - record.adjustment_cr
+                    )
+                else:
+                    record.adjusted_balance = (
+                        record.current_balance
+                        - record.adjustment_dr
+                        + record.adjustment_cr
+                    )
