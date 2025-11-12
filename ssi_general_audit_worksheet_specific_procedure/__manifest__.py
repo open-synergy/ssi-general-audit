563e77d5ca09fde201b7b66601338ba97b337b4a
Andhitia Rama <andhitia.r@gmail.com>
2025-11-12 15:29:47 +0700

    [UPD] ssi_general_audit_worksheet_specific_procedure
    * Menambahkan data untuk goiung concern

--- ssi_general_audit_worksheet_specific_procedure/__manifest__.py
+++ ssi_general_audit_worksheet_specific_procedure/__manifest__.py
@@ -22,6 +22,8 @@
         "data/master/general_audit_ws_ee819ae_item.xml",
         "data/master/general_audit_subsequent_event.xml",
         "data/master/related_party_confirmation_procedure.xml",
+        "data/master/general_audit_going_concern_z_score_coeficient_set.xml",
+        "data/master/trial_balance_computation_item.xml",
         # a8f4d88 - Accounting Estimation
         "security/res_group/general_audit_ws_a8f4d88.xml",
         "security/ir_rule/general_audit_ws_a8f4d88.xml",
