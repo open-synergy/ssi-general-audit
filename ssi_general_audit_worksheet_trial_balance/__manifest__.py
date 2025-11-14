2f8ccb07e0216d90741bfe69c9621c901b5d34ca
mikevhe18 <viriyananda.michael@gmail.com>
2025-11-14 11:29:21 +0700

    [ADD][ssi_general_audit_worksheet_trial_balance] Master Data Conclusion

--- ssi_general_audit_worksheet_trial_balance/__manifest__.py
+++ ssi_general_audit_worksheet_trial_balance/__manifest__.py
@@ -16,6 +16,7 @@
         "security/ir_module_category_data.xml",
         "data/general_audit_worksheet_type_data.xml",
         # MASTER DATA
+        "data/master/general_audit_worksheet_conclusion.xml",
         "data/master/general_audit_ws_a033cc6_item.xml",
         # a033cc6
         "security/res_group/general_audit_ws_a033cc6.xml",
