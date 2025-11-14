a875013e713efbdb98162dde80c74c774be419c3
mikevhe18 <viriyananda.michael@gmail.com>
2025-11-14 10:57:19 +0700

    [ADD][ssi_general_audit_worksheet_team_communication] Master Data Conclusion

--- ssi_general_audit_worksheet_team_communication/__manifest__.py
+++ ssi_general_audit_worksheet_team_communication/__manifest__.py
@@ -16,6 +16,7 @@
         "security/ir_module_category_data.xml",
         "data/general_audit_worksheet_type_data.xml",
         # MASTER DATA
+        "data/master/general_audit_worksheet_conclusion.xml",
         "data/master/general_audit_ws_437fc8f_item.xml",
         "data/master/general_audit_ws_b1f820c_item.xml",
         # 437fc8f - Team Communication Pre-Engagement
