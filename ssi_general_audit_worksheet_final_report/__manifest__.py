8487afd6c8d299f86c17e6699a43c6b6d81f5314
mikevhe18 <viriyananda.michael@gmail.com>
2025-11-17 07:41:42 +0700

    [ADD][ssi_general_audit_worksheet_final_report] Master Data Conclusion

--- ssi_general_audit_worksheet_final_report/__manifest__.py
+++ ssi_general_audit_worksheet_final_report/__manifest__.py
@@ -15,6 +15,8 @@
     "data": [
         "security/ir_module_category_data.xml",
         "data/general_audit_worksheet_type_data.xml",
+        # MASTER DATA
+        "data/master/general_audit_worksheet_conclusion.xml",
         # a8c54f3
         "security/res_group/general_audit_ws_a8c54f3.xml",
         "security/ir_rule/general_audit_ws_a8c54f3.xml",
