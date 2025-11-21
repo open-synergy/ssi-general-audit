b86612151a3421a5770774a3e054aa892ae105a9
Andhitia Rama <andhitia.r@gmail.com>
2025-11-21 19:56:35 +0700

    [UPD] ssi_general_audit_worksheet_draft_reporting
    * Menambahkan opini dan tanggal opini

--- ssi_general_audit_worksheet_draft_reporting/models/ga_ff42fdc/general_audit_ws_ff42fdc.py
+++ ssi_general_audit_worksheet_draft_reporting/models/ga_ff42fdc/general_audit_ws_ff42fdc.py
@@ -2,7 +2,7 @@
 # Copyright 2025 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
 
-from odoo import models
+from odoo import fields, models
 
 
 class GeneralAuditWSff42fdc(models.Model):
@@ -13,4 +13,24 @@ class GeneralAuditWSff42fdc(models.Model):
     ]
     _type_xml_id = (
         "ssi_general_audit_worksheet_draft_reporting." "worksheet_type_ff42fdc"
+    )
+
+    financial_statement_opinion_id = fields.Many2one(
+        comodel_name="accountant.opinion",
+        string="Opinion on Financial Statement",
+        related="general_audit_id.opinion_id",
+        store=True,
+        readonly=False,
+        states={
+            "open": [("readonly", False)],
+        },
+    )
+    financial_statement_opinion_date = fields.Date(
+        string="Date of Opinion on Financial Statement",
+        related="general_audit_id.opinion_date",
+        store=True,
+        readonly=False,
+        states={
+            "open": [("readonly", False)],
+        },
     )
