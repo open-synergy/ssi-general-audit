57b6e453f4543db32aa46a8587e62fb68115face
Andhitia Rama <andhitia.r@gmail.com>
2026-02-13 20:36:27 +0700

    [ADD] ssi_general_audit_worksheet_audit_procedure_confirmation

---
+++ ssi_general_audit_worksheet_audit_procedure_confirmation/__manifest__.py
@@ -0,0 +1,31 @@
+# Copyright 2025 OpenSynergy Indonesia
+# Copyright 2025 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "General Audit Worksheet - Reperformance Audit Procedure",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_general_audit",
+        "ssi_general_audit_worksheet_control_risk",
+        "ssi_general_audit_worksheet_lead_schedule",
+        "ssi_general_audit_worksheet_understanding_entity",
+    ],
+    "data": [
+        "security/ir_module_category/general_audit_ws_d45dd19.xml",
+        "security/res_groups/general_audit_ws_d45dd19.xml",
+        "security/ir_model_access/general_audit_ws_d45dd19.xml",
+        "security/ir_rule/general_audit_ws_d45dd19.xml",
+        "data/ir_sequence/general_audit_ws_d45dd19.xml",
+        "data/sequence_template/general_audit_ws_d45dd19.xml",
+        "data/policy_template/general_audit_ws_d45dd19.xml",
+        "data/approval_template/general_audit_ws_d45dd19.xml",
+        "data/general_audit_worksheet_type/general_audit_ws_d45dd19.xml",
+        "views/general_audit_ws_d45dd19.xml",
+    ],
+    "demo": [],
+}
