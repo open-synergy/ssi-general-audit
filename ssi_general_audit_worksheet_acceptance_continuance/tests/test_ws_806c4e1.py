bdaa44616b953be4003b68b7dca9d194627f2509
Andhitia Rama <andhitia.r@gmail.com>
2026-07-13 18:25:33 +0700

    [UPD] ssi_general_audit_worksheet_acceptance_continuance
    Menambahkan unit test skenario YAML (odoo-yaml-test) untuk ketujuh
    worksheet modul ini: general_audit_ws_0427d28, _369c5a5, _805d4d5,
    _806c4e1, _842f0d6, _b9d8a5c, dan _f5e7049. Mencakup create, workflow
    draft-open-confirm-done, cancel/restart, seluruh field compute (normal
    dan kosong), action_reload_links, action_populate_personnel,
    action_create_summary, onchange lewat action:form, serta CRUD
    master/checklist item.

---
+++ ssi_general_audit_worksheet_acceptance_continuance/tests/test_ws_806c4e1.py
@@ -0,0 +1,12 @@
+# Copyright 2026 PT. Open Source Integra Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+
+from odoo_yaml_test import YamlTransactionCase
+
+from odoo.tests import tagged
+
+
+@tagged("post_install", "-at_install")
+class TestGeneralAuditWs806c4e1(YamlTransactionCase):
+    def test_general_audit_ws_806c4e1(self):
+        self.run_yaml_scenario("test_data_ws_806c4e1.yaml")
