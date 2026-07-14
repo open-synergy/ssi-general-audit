ea52fff3d7bb8596942862b84e7bb1ec0c5d09da
Andhitia Rama <andhitia.r@gmail.com>
2026-07-14 12:07:09 +0700

    [UPD] ssi_general_audit_worksheet_audit_result
    Menambahkan unit test berbasis skenario YAML (odoo-yaml-test) untuk
    keempat worksheet fase Reporting: a0319a2 (Findings That Influence
    Opinion), d33420f (Control Deficiencies), bc3e272 (Audit Result
    Discussion), dan ab19fd4 (Audit Result Formulation).
    
    * Siklus workflow draft -> open -> confirm -> approve -> done, plus
      cancel/restart untuk keempat worksheet.
    * action_populate pada bc3e272: kondisi normal, idempoten (dipanggil
      dua kali), sumber kosong, dan penghapusan baris basi saat sumbernya
      berpindah keluar dari general_audit yang sama.
    * Compute link_1_ids/link_2_ids pada ab19fd4 untuk kondisi normal dan
      kosong, serta action_recompute yang mencerminkan detail baru.
    * CRUD model baris (.detail, .influence, .control) dengan field
      worksheet_id wajib terisi.
    
    Seluruh fixture dibuat melalui action: create (tanpa data demo).

---
+++ ssi_general_audit_worksheet_audit_result/tests/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2026 OpenSynergy Indonesia
+# Copyright 2026 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+
+from . import test_ws_a0319a2
+from . import test_ws_d33420f
+from . import test_ws_bc3e272
+from . import test_ws_ab19fd4
---
+++ ssi_general_audit_worksheet_audit_result/tests/test_ws_a0319a2.py
@@ -0,0 +1,13 @@
+# Copyright 2026 OpenSynergy Indonesia
+# Copyright 2026 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+
+from odoo_yaml_test import YamlTransactionCase
+
+from odoo.tests import tagged
+
+
+@tagged("post_install", "-at_install")
+class TestWSA0319A2(YamlTransactionCase):
+    def test_ws_a0319a2(self):
+        self.run_yaml_scenario("test_data_ws_a0319a2.yaml")
