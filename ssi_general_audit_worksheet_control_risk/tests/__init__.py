f8a7540f7bee938b8539b0fb57926ef7e8c30624
Andhitia Rama <andhitia.r@gmail.com>
2026-07-14 15:41:30 +0700

    [UPD] ssi_general_audit_worksheet_control_risk
    * Menambahkan unit test tests/ berbasis odoo-yaml-test untuk modul ini,
      yang sebelumnya belum memiliki test sama sekali.
    * Mencakup CRUD ke-12 model master (general/IT control category, control,
      indicator, set; control activity; assersion type; key internal control
      business cycle & significant account) beserta compute
      general_control_ids/it_control_ids.
    * Mencakup workflow draft->open->confirm->approve->done plus cancel/restart
      untuk keenam worksheet (d3d2719, f63f569, b59b886, eabdaad, ba9b2f0,
      a58e29c), action_load_detail (d3d2719 & f63f569, termasuk idempotensi dan
      set_id kosong), action_populate (b59b886, termasuk idempotensi dan sumber
      kosong), dan action_reload_links (a58e29c) beserta compute link_1_id/
      link_2_ids/link_3_id.
    * Mencakup compute result & standard_detail_ids/standard_detail_id pada
      detail eabdaad dan ba9b2f0 (kondisi normal & kosong).

---
+++ ssi_general_audit_worksheet_control_risk/tests/__init__.py
@@ -0,0 +1,11 @@
+# Copyright 2026 OpenSynergy Indonesia
+# Copyright 2026 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (
+    test_master_data,
+    test_ws_control_evaluation,
+    test_ws_b59b886,
+    test_ws_internal_control,
+    test_ws_a58e29c,
+)
