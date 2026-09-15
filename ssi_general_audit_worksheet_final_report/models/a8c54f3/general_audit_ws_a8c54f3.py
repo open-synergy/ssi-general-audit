933ba9d8d2e80b84f9f26ab9b80a2a99264eb3e8
mikevhe18 <viriyananda.michael@gmail.com>
2026-09-15 19:45:43 +0700

    [ADD] ssi_general_audit_worksheet_final_report: field Proposed Audit Opinion
    Tambah field proposed_audit_opinion_id (m2o accountant.opinion) di KKA
    Audit Final Memorandum, tampil di atas field Conclusion. Closes #342.

--- ssi_general_audit_worksheet_final_report/models/a8c54f3/general_audit_ws_a8c54f3.py
+++ ssi_general_audit_worksheet_final_report/models/a8c54f3/general_audit_ws_a8c54f3.py
@@ -2,7 +2,7 @@
 # Copyright 2025 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
 
-from odoo import models
+from odoo import fields, models
 
 
 class GeneralAuditWSa8c54f3(models.Model):
@@ -37,3 +37,10 @@ class GeneralAuditWSa8c54f3(models.Model):
         "general_audit_worksheet_mixin",
     ]
     _type_xml_id = "ssi_general_audit_worksheet_final_report." "worksheet_type_a8c54f3"
+
+    proposed_audit_opinion_id = fields.Many2one(
+        string="Proposed Audit Opinion",
+        comodel_name="accountant.opinion",
+        help="Audit opinion proposed by the engagement partner prior to "
+        "issuing the final auditor's report.",
+    )
